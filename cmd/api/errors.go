03720a9720e71b613ce519cc4ee9dd1ef3cbbc59
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2024-06-25 18:39:04 -0400

    Implement login endpoint

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -1,57 +1,25 @@
 package main
 
-import (
-	"errors"
-	"net/http"
-
-	"cloud.google.com/go/logging"
-	"github.com/marcusmonteirodesouza/realworld-backend-go-jet-postgresql/internal/services"
-)
-
-type errorResponse struct {
-	Errors errorResponseErrors `json:"errors"`
+type forbiddenError struct {
+	msg string
 }
 
-type errorResponseErrors struct {
-	Body []string `json:"body"`
+func (err *forbiddenError) Error() string {
+	return err.msg
 }
 
-func (app *application) writeErrorResponse(w http.ResponseWriter, err error) {
-	app.logger.StandardLogger(logging.Error).Print(err.Error())
+type malformedRequest struct {
+	msg string
+}
 
-	var msg string
-	var status int
+func (err *malformedRequest) Error() string {
+	return err.msg
+}
 
-	var alreadyExistsError *services.AlreadyExistsError
-	var invalidArgumentError *services.InvalidArgumentError
+type unauthorizedError struct {
+	msg string
+}
 
-	var malformedRequest *malformedRequest
-
-	switch {
-	case errors.As(err, &alreadyExistsError):
-		msg = err.Error()
-		status = http.StatusConflict
-
-	case errors.As(err, &invalidArgumentError):
-		msg = err.Error()
-		status = http.StatusUnprocessableEntity
-
-	case errors.As(err, &malformedRequest):
-		msg = malformedRequest.Error()
-		status = http.StatusBadRequest
-
-	default:
-		msg = "Internal server error"
-		status = http.StatusInternalServerError
-	}
-
-	err = writeJSON(w, status, errorResponse{
-		Errors: errorResponseErrors{
-			Body: []string{msg},
-		},
-	}, nil)
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+func (err *unauthorizedError) Error() string {
+	return err.msg
 }
